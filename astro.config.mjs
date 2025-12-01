6bdfe4d50e053ee014bc2364852b797511c81398
Victoria Ritvo <ritvo.v@gmail.com>
2025-12-01 14:15:48 -0500

    update astro.config.mjs for prod/dev mode

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+// Check if production mode
+const isProd = process.env.NODE_ENV === 'production';
+
 // https://astro.build/config
-export default defineConfig({});
+
+// Set different site / base depending on if in production / dev
+export default defineConfig({
+    site: isProd ? 'https://vritvo.github.io' : 'http://localhost:4321',
+    base: isProd ? '/vr_website' : '/',
+});
