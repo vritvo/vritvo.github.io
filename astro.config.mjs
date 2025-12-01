c64dae784bccb4f6bd9e5bc7defe436aab5081b7
Victoria Ritvo <ritvo.v@gmail.com>
2025-12-01 14:58:14 -0500

    removing all the base paths

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,7 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-// Check if production mode
-const isProd = process.env.NODE_ENV === 'production';
-
 // https://astro.build/config
-
-// Set different site / base depending on if in production / dev
 export default defineConfig({
-    site: isProd ? 'https://vritvo.github.io' : 'http://localhost:4321',
-    base: isProd ? '/vr_website' : '/',
+    site: 'https://vritvo.github.io',
 });
